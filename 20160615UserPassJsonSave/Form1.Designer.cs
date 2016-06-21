175647f44a5ac0be02d5d7e8cca9fade900996d2
ug-code <ugurgunes@windowslive.com>
2016-06-21 10:18:24 +0300

    Add files via upload

---
+++ 20160615UserPassJsonSave/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _20160615UserPassSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Showbutton = new System.Windows.Forms.Button();
+            this.Hidebutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(768, 189);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save txt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Showbutton
+            // 
+            this.Showbutton.BackColor = System.Drawing.Color.SeaGreen;
+            this.Showbutton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Showbutton.Location = new System.Drawing.Point(509, 248);
+            this.Showbutton.Name = "Showbutton";
+            this.Showbutton.Size = new System.Drawing.Size(109, 32);
+            this.Showbutton.TabIndex = 3;
+            this.Showbutton.Text = "Show Pass";
+            this.Showbutton.UseVisualStyleBackColor = false;
+            this.Showbutton.Click += new System.EventHandler(this.Showbutton_Click);
+            // 
+            // Hidebutton
+            // 
+            this.Hidebutton.BackColor = System.Drawing.Color.Maroon;
+            this.Hidebutton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Hidebutton.Location = new System.Drawing.Point(509, 211);
+            this.Hidebutton.Name = "Hidebutton";
+            this.Hidebutton.Size = new System.Drawing.Size(109, 32);
+            this.Hidebutton.TabIndex = 4;
+            this.Hidebutton.Text = "Hide Pass";
+            this.Hidebutton.UseVisualStyleBackColor = false;
+            this.Hidebutton.Click += new System.EventHandler(this.Hidebutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Author: Ug-Code";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(768, 357);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Hidebutton);
+            this.Controls.Add(this.Showbutton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "User&Pass Logger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Showbutton;
+        private System.Windows.Forms.Button Hidebutton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 20160615UserPassJsonSave/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _20160615UserPassSave
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        string username = ""; string password = ""; string result = ""; string website = "";
+        bool visibility;
+        public Form1()
+        {
+
+            InitializeComponent();
+            table = new DataTable();
+            table.Columns.Add("Username", typeof(string));
+            table.Columns.Add("Password", typeof(string));
+            table.Columns.Add("WebSite", typeof(string));
+            dataGridView1.DataSource = table;
+            try
+            {
+                txtRead(visibility = false);
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        //Save Txt
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Are you sure you want to saved.", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (d == DialogResult.Yes)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    username = row["Username"].ToString();
+                    password = row["Password"].ToString();
+
+                    website = row["WebSite"].ToString();
+                    result += username + ";" + password + ";" + website + "\r\n";
+                }
+
+                if (password == "******" || password == "" || password == null || password == " ")
+                {
+                    MessageBox.Show("ShowPass Button click,Empty pass and try");
+                }
+                else
+                {
+                    using (TextWriter tw = new StreamWriter("file.txt"))
+                    {
+                        try
+                        {
+                            tw.WriteLine(result);
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Unable to write to a text file");
+                        }
+
+                    }
+                    table.Clear();
+                    Application.Restart();
+                }
+            }
+
+        }
+
+        void txtRead(bool visibility = true)
+        {
+            string line; string[] words; string user = ""; string pass = ""; string web = "";
+            using (StreamReader sr = new StreamReader("file.txt"))
+            {
+                // Read and display lines from the file until the end of 
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        words = line.Split(';');
+                        user = words[0];
+                        pass = words[1];
+                        web = words[2];
+                        if (visibility == true)
+                            table.Rows.Add(new string[] { user, pass, web });
+                        else
+                            table.Rows.Add(new string[] { user, "******", web });
+                    }
+                    catch (Exception) { }
+                }
+            }
+        }
+
+
+        #region ButtonEvent
+
+        //Show Pass
+        private void Showbutton_Click(object sender, EventArgs e)
+        {
+            table.Clear();
+            try
+            {
+                txtRead();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show(" empty table :) or txtRead() method failed");
+            }
+
+        }
+        //hide Pass
+        private void Hidebutton_Click(object sender, EventArgs e)
+        {
+            table.Clear();
+            try
+            {
+                txtRead(visibility = false);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("empty table :) or txtRead() Method Failed");
+            }
+
+        }
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
